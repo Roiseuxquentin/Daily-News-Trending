56bd261765a8897da89dc5ead8bc3e2f36760a95
roiseux quentin <quentin@roiseux.fr>
2019-12-28 15:09:01 +0100

    add moment of day picture

--- androidProject/components/Home.js
+++ androidProject/components/Home.js
@@ -1,36 +1,48 @@
-import React, {Component} from 'react';
-import { View, Image,TouchableNativeFeedback } from 'react-native';
+import React, {Component} from 'react'
+import { View, Image,TouchableNativeFeedback, Text } from 'react-native'
 
-import KHmating from '../ressources/images/kamehouseMATIN.jpg';
-import cloudIMG from '../ressources/images/dbzCloud.png';
+import KHMATIN from '../ressources/images/kamehouseMATIN.jpg'
+import KHJOURNEE from '../ressources/images/kamehouseJOURNEE.jpg'
+import KHSOIREE from '../ressources/images/kamehouseSOIREE.jpg'
+
+import cloudIMG from '../ressources/images/dbzCloud.png'
 
 class Home extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
+			moment : require('../ressources/images/kamehouseSOIREE.jpg') ,
 			data : null,
 		}
 	}
 
+	componentDidMount() {
+		const day = new Date()
+		const Heure = day.getHours()
+
+		if (( Heure > 10 ) && ( Heure < 18)) {
+			this.setState({moment : require('../ressources/images/kamehouseJOURNEE.jpg') })
+		} else if ( Heure < 10 )  {
+			this.setState({moment : require('../ressources/images/kamehouseMATIN.jpg') })
+		} else {
+			this.setState({moment : require('../ressources/images/kamehouseSOIREE.jpg') })
+		} 
+	}
+
 	render() {
 		return (
-			<View style={{grid: 1}} >
-				<View style={{flex: 1, flexDirection : 'row', justifyContent : 'center' }} >
+			<View style={{grid: 1, gridDirection: 'column', justifyContent: 'center', alignItems: 'center',marginTop : 2}} >
 					<TouchableNativeFeedback onPress={() => console.log('ok') } > 
-						<Image source={KHmating} 
-						style={{ height : 250, width : 200, marginTop : 5  }} />
+							<Image source={this.state.moment} style={{ height : 300, width : '98%', }} />
 					</TouchableNativeFeedback> 
-				</View>
-
+					<Text>LE GREC</Text>
 				<View style={{flex: 1, flexDirection : 'row', justifyContent : 'center' }} >
 					<TouchableNativeFeedback onPress={() => console.log('ok') } > 
-						<Image source={cloudIMG} 
-						style={{ height : 60, width : 60, marginTop : 5  }} />
+						<Image source={cloudIMG} style={{ height : 60, width : 60, marginTop : 5  }} />
 					</TouchableNativeFeedback> 
 
 					<TouchableNativeFeedback onPress={() => console.log('ok') } > 
-						<Image source={cloudIMG} 
-						style={{ height : 60, width : 60, marginTop : 5  }} />
+						<Image source={cloudIMG} style={{ height : 60, width : 60, marginTop : 5  }} />
 					</TouchableNativeFeedback> 
 				</View>
 			</View>
@@ -38,4 +50,4 @@ class Home extends Component {
 	}
 }
 
-export default Home;
+export default Home
